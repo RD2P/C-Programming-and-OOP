09f192017b302eeaab2a4c3c80527eb4b8868f96
Raphael <graphael.dr@gmail.com>
2024-10-14 09:00:17 -0600

    printing, inputs

---
+++ ch1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+  
+  // printf("%d\n", 1 + 1);
+  // printf("x %.1f i %d\n", 3.5, 100);
+
+  // char num = 5;
+  // printf("%05d hey", num);
+
+  // using wrong specifier, result in 0.0
+  // printf("%.1f",8); 
+  // binary representation of int 8 when interpreted as float is very small
+
+  // will result in unexpected behaviour, will need one space at the end for end of string indicator
+  // char name[3] = "cat";
+  // printf("%s", name);
+
+  // scanf
+  /*
+  int num;
+  scanf("%d", &num);
+  printf("%d", num);
+  */
+  
+  /*
+  // notice we don't need & when scanning string 
+  char name[100];
+  printf("Enter your name: ");
+  scanf("%100s", name);
+  printf("Hello %s", name);
+  */
+  
+
+}
+
+/*
+char surrounded by ''
+char array surrounded by ""
+NULL is number 0, cast to be pointer to nothing
+*/
