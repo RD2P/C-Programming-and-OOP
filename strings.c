79eebcbdd503cbf4869f0100771bb0eb13ce1c51
Raphael <graphael.dr@gmail.com>
2024-11-01 23:38:28 -0600

    strings

---
+++ strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  // strings
+  char name[10] = "Diablo";
+  printf("%s\n", &name);
+
+  // print char as number to get ascii code
+  printf("%c %d", 'A', 'A');
+
+
+  return 0;
+}
