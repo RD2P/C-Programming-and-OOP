cdbaf9c52738d78d2b77bbe49eb595b4cdc097ea
Raphael <graphael.dr@gmail.com>
2024-11-02 00:07:34 -0600

    move some code to own files

--- ch1.c
+++ ch1.c
@@ -16,46 +16,10 @@ int main() {
   // char name[3] = "cat";
   // printf("%s", name);
 
-  // scanf
-  /*
-  int num;
-  scanf("%d", &num);
-  printf("%d", num);
-  */
-  
-  /*
-  // notice we don't need & when scanning string 
-  char name[100];
-  printf("Enter your name: ");
-  scanf("%100s", name); // limit: only read up to 100 characters
-  printf("Hello %s", name);
-  */
-  
-  // scan a line
-  /*
-  char line[1000];
-  printf("Enter a line:\n");
-  scanf("%[^\n]1000s", line);
-  printf("%s",line);
-  */
-
-  // reading a file
-  /*
-  char line[1000];
-  FILE *hand; //pointer to the file object
-  hand = fopen("words.txt", "r");
-  while( fgets(line, 1000, hand) != NULL){
-    printf("%s", line);
-  }
-  */
-
   // for loop
   // for (int i = 0; i < 5; i++){
   //   printf("%d", i);
   // }
-
-
-
 }
 
 /*
