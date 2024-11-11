5fc7f7ad7cec8abbe75add3c943a81aafc49167d
Raphael <graphael.dr@gmail.com>
2024-11-10 23:50:31 -0600

    count characters, words, and lines

--- char_count.c
+++ char_count.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 int main(){
+  // int cc = 0;
+
+  // while(getchar() != EOF){
+  //   printf("%d\n",cc);
+  //   cc++;
+  // }
+  // printf("%d", cc);
+
+  /*Count chars, words, lines*/
+  int cl = 0;
+  int cw = 0;
+  int c = 0;
   int cc = 0;
-
-  while(getchar() != EOF){
-    printf("%d\n",cc);
-    cc++;
+  while((c = getchar()) != EOF){
+    ++cc;
+    if (c == '\n'){
+      ++cl;
+      ++cw;
+    }
+    if (c == ' ' || c == '\t' ){
+      ++cw;
+    }
   }
-  printf("%d", cc);
-
+  printf("Char: %d\tWord: %d\tLine: %d\n", cc, cw, cl);
   return 0;
 }
